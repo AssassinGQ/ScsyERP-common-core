430fbfb4c4840f65e4b83d04342dee4b3c60af16
AssassinG <hangq_zju@hotmail.com>
2018-12-17 16:51:26 +0800

    push

--- src/main/java/cn/AssassinG/ScsyERP/common/core/biz/FormBiz.java
+++ src/main/java/cn/AssassinG/ScsyERP/common/core/biz/FormBiz.java
@@ -3,6 +3,7 @@ package cn.AssassinG.ScsyERP.common.core.biz;
 import cn.AssassinG.ScsyERP.common.entity.FormEntity;
 
 public interface FormBiz<T extends FormEntity> extends BaseBiz<T> {
+    //todo create的时候需要检查仓库是否已经存在一个活跃的出库单
     void update(T formEntity);
     void complete(Long entityId);
 }
