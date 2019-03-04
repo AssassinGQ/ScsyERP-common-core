d20cb2985358e683cc3318019ee0d4278a71753c
AssassinG <hangq_zju@hotmail.com>
2019-03-04 14:36:31 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/common/core/biz/impl/UnLoginableBizImpl.java
+++ src/main/java/cn/AssassinG/ScsyERP/common/core/biz/impl/UnLoginableBizImpl.java
@@ -2,20 +2,19 @@ package cn.AssassinG.ScsyERP.common.core.biz.impl;
 
 import cn.AssassinG.ScsyERP.common.core.biz.UnLoginableBiz;
 import cn.AssassinG.ScsyERP.common.entity.UnLoginableEntity;
-import cn.AssassinG.ScsyERP.common.exceptions.BizException;
 
 public abstract class UnLoginableBizImpl<T extends UnLoginableEntity> extends BaseBizImpl<T> implements UnLoginableBiz<T> {
 
-    @Override
-    public Long create(T entity) {
-        throw new BizException(000000, "不可登录基本信息不能使用create方法创建");
-    }
+//    @Override
+//    public Long create(T entity) {
+//        throw new BizException(000000, "不可登录基本信息不能使用create方法创建");
+//    }
 
     public Long createWithNameCheck(T entity){
         if(entity.getName() == null || entity.getName().isEmpty()){
             entity.setName("-1");
         }
-        Long id = super.create(entity);
+        Long id = this.create(entity);
         if(entity.getName().equals("-1")){
             entity.setName(entity.getClass().getSimpleName()+id);
             this.update(entity);
